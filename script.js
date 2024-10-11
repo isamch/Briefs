d19cc9e39ce4c56c3eedb06b883d0748e4378fe9
isam chajia <isamchajiabeat.com>
2024-10-11 15:25:46 +0100

    first brief

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggledown() {
+      const navbar = document.getElementById('nav_barm');
+      const button_nav = document.getElementById('btn_bar');
+
+      if (navbar.style.transform == 'translateY(-100%)') {
+            navbar.style.transform = 'translateY(0%)';
+            button_nav.style.transform = 'scaleY(-1)';
+      }else{
+            navbar.style.transform = 'translateY(-100%)';
+            button_nav.style.transform = 'scaleY(1)';
+
+      }
+      
+
+}
