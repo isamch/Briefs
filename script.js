e566d54d6b2a5fdcb6224d2e17d70090b6554c65
isam chajia <isamchajiabeat.com>
2024-10-11 17:21:45 +0100

    hazd

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function toggledown() {
       const navbar = document.getElementById('nav_barm');
       const button_nav = document.getElementById('btn_bar');
 
-      if (navbar.style.transform == 'translateY(-100%)') {
+      if (navbar.style.transform == 'translateY(-40%)') {
             navbar.style.transform = 'translateY(0%)';
             button_nav.style.transform = 'scaleY(-1)';
       }else{
-            navbar.style.transform = 'translateY(-100%)';
+            navbar.style.transform = 'translateY(-40%)';
             button_nav.style.transform = 'scaleY(1)';
 
       }
