b4a11028026c8afba8bc65206ddefcf2d1a2e6dd
isam chajia <isamchajiabeat.com>
2024-10-11 23:40:03 +0100

    fin

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function toggledown() {
-      const navbar = document.getElementById('nav_barm');
-      const button_nav = document.getElementById('btn_bar');
+	const navbar = document.getElementById('nav_barm');
+	const button_nav = document.getElementById('btn_bar');
 
-      if (navbar.style.transform == 'translateY(-40%)') {
-            navbar.style.transform = 'translateY(0%)';
-            button_nav.style.transform = 'scaleY(-1)';
-      }else{
-            navbar.style.transform = 'translateY(-40%)';
-            button_nav.style.transform = 'scaleY(1)';
+	if (navbar.style.transform == 'translateY(-40%)') {
+		navbar.style.transform = 'translateY(0%)';
+		button_nav.style.transform = 'scaleY(-1)';
+	} else {
+		navbar.style.transform = 'translateY(-40%)';
+		button_nav.style.transform = 'scaleY(1)';
+	}
 
-      }
-      
 
 }
